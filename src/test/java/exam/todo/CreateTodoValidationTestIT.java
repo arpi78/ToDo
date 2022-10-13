906270526e7e3d7ad4fb4fef86727489368f3304
arpi78 <ahimpli@gmail.com>
2022-10-13 13:13:00 +0200

    h2 teszt db, docker fájlok

--- src/test/java/exam/todo/CreateTodoValidationTestIT.java
+++ src/test/java/exam/todo/CreateTodoValidationTestIT.java
@@ -12,8 +12,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import javax.validation.ConstraintViolation;
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -27,6 +29,15 @@ public class CreateTodoValidationTestIT {
 
         @Test
         void testCreateToDo(){
+
+            webClient
+                    .post()
+                    .uri( "/api/users")
+                    .bodyValue(new CreateUserCommand("johne@gmail.com"))
+                    .exchange()
+                    .expectStatus().isCreated();
+
+
             var result = webClient
                     .post()
                     .uri("/api/users/1/todos")
